a02d7ccd90b98845bc0ffb444b25024ad547e2f7
tanag3r <luke.wylie@comcast.net>
2022-02-14 16:44:28 -0800

    new ground

--- test_unitTest.py
+++ test_unitTest.py
@@ -1,7 +1,4 @@
-import pytest
-
 def test_assertTrue():
     assert True
-from hotspotsTable_explore.i import readTrailheads_fromDB
-def test_readFromDB():
-    readTrailheads_fromDB()
+def test_geoNullValues():
+    trailheadRef['']
