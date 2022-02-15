54b4ed155293078b57fe675b741016f4b8ab3555
tanag3r <luke.wylie@comcast.net>
2022-02-14 16:11:15 -0800

    removing trash

--- test_unitTest.py
+++ test_unitTest.py
@@ -2,6 +2,6 @@ import pytest
 
 def test_assertTrue():
     assert True
-from hotspotsTable_explore import readTrailheads_fromDB
+from hotspotsTable_explore.i import readTrailheads_fromDB
 def test_readFromDB():
     readTrailheads_fromDB()
