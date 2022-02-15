703909eaf7952f77ed4a2d6fef64252b653ec395
tanag3r <luke.wylie@comcast.net>
2022-02-15 15:13:34 -0800

    unit tests are frustrating

--- tests/test_hotspots.py
+++ tests/test_hotspots.py
@@ -1,11 +1,9 @@
 ##https://testbook.readthedocs.io/en/latest/usage/index.html#set-up-jupyter-notebook-under-test
 
 import pandas as pd
-from sqlalchemy import true
 from testbook import testbook
 
-@testbook('hotspotsTable_explore.ipynb',execute=True)
-def test_foo(tb):
-    foo = tb.ref("foo")
-
-    assert foo(2) == 3
+@testbook('hotspots_cooking.ipynb',execute=True)
+def test_getHotspots(tb):
+    TrailheadRef = tb.ref("getTrailheadRef")
+    assert TrailheadRef == pd.DataFrame()
