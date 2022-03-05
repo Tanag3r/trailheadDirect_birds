d39fc95abb3e4e4a443c4fb6b8797365ee624d45
tanag3r <luke.wylie@comcast.net>
2022-03-04 16:07:16 -0800

    FAO_by_stopName ready

--- sqlite_scripts/createTable_FAObyStopName.sql
+++ sqlite_scripts/createTable_FAObyStopName.sql
@@ -1,24 +1,27 @@
 --Cooked FAO land cover type for each stopName
+CREATE VIEW 'FAO_by_stopName'
+AS
+
 select 
 	--Hotspots.locName,
 	--Hotspots.locId,
 	Hotspots.StopName,
-	MCD12Q1Prop1.Date,
-	avg(MCD12Q1Prop1."(1) Barren") as 'Barren',
-	avg(MCD12Q1Prop1."(3) Water Bodies") as 'Water Bodies',
-	avg(MCD12Q1Prop1."(11) Evergreen Needleleaf Forest") as 'Evergreen Needleleaf Forest',
-	avg(MCD12Q1Prop1."(12) Evergreen Broadleaf Forests") as 'Evergreen Broadleaf Forests',
-	avg(MCD12Q1Prop1."(21) Open Forests") as 'Open Forests',
-	avg(MCD12Q1Prop1."(15) Mixed Broadleaf/Needleleaf Forests") as 'Mixed Broadleaf/Needleleaf Forests',
-	avg(MCD12Q1Prop1."(22) Sparse Forests") as 'Spare Forests',
-	avg(MCD12Q1Prop1."(31) Dense Herbaceous") as 'Dense Herbaceous',
-	avg(MCD12Q1Prop1."(32) Sparse Herbaceous") as 'Sparse Herbaceous',
-	avg(MCD12Q1QAQC."0") as 'cleanPixels'
+	--MCD12Q1Prop1.Date,
+	sum(MCD12Q1Prop1."(1) Barren")/3 as 'Barren',
+	sum(MCD12Q1Prop1."(3) Water Bodies")/3 as 'Water Bodies',
+	sum(MCD12Q1Prop1."(11) Evergreen Needleleaf Forest")/3 as 'Evergreen Needleleaf Forest',
+	sum(MCD12Q1Prop1."(12) Evergreen Broadleaf Forests")/3 as 'Evergreen Broadleaf Forests',
+	sum(MCD12Q1Prop1."(21) Open Forests")/3 as 'Open Forests',
+	sum(MCD12Q1Prop1."(15) Mixed Broadleaf/Needleleaf Forests")/3 as 'Mixed Broadleaf/Needleleaf Forests',
+	sum(MCD12Q1Prop1."(22) Sparse Forests")/3 as 'Spare Forests',
+	sum(MCD12Q1Prop1."(31) Dense Herbaceous")/3 as 'Dense Herbaceous',
+	sum(MCD12Q1Prop1."(32) Sparse Herbaceous")/3 as 'Sparse Herbaceous',
+	sum(MCD12Q1QAQC."0")/3 as 'cleanPixels'
 from Hotspots 
 
 left outer join "MCD12Q1-006-LC-Prop1-Statistics.csv_MODIS_cooking" as MCD12Q1Prop1 on MCD12Q1Prop1.locId = Hotspots.locId
 left outer join "MCD12Q1-006-QC-Statistics-QA.csv_MODIS_cooking" as MCD12Q1QAQC on MCD12Q1Prop1.locId = MCD12Q1QAQC.locId
 
 --where StopName = 'MargaretsWay'
-group by StopName,MCD12Q1Prop1.Date
+group by StopName
 ;
