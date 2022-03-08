05889c0b0f41ad3997ef7bf871ae0776636c771d
tanag3r <luke.wylie@comcast.net>
2022-03-08 12:53:39 -0800

    coefficients view for species and hotspots

---
+++ sqlite_scripts/createView_coefficientsBySpecies.sql
@@ -0,0 +1,14 @@
+CREATE VIEW 'coefficients_bySpecies' AS
+select
+	comName,
+	locId,
+	count(subId) as 'uniqueSightings',
+	round(avg(howMany),2) as 'averageVolume',
+	--strftime('%Y',obsDt) as 'year',
+	(SELECT COUNT(DISTINCT(strftime('%Y',obsDt))) FROM historicObservations as hz WHERE hz.comName=HsOb.comName and hz.locId=HsOb.locId) as 'YEARfrequency',
+	(SELECT COUNT(DISTINCT(strftime('%W',obsDt))) FROM historicObservations as hz WHERE hz.comName=HsOb.comName and hz.locId=HsOb.locId) as 'WEEKfrequency'
+
+from historicObservations as HsOb
+
+group by comName,locId--,strftime('%Y',obsDt)
+;
