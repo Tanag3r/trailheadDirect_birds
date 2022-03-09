6f86b3e68ce3bd01fe4f8e2588c353012ff3d35b
tanag3r <luke.wylie@comcast.net>
2022-03-09 13:18:04 -0800

    issue 70 closed

--- sqlite_scripts/createTable_FAObyLocId.sql
+++ sqlite_scripts/createTable_FAObyLocId.sql
@@ -7,6 +7,9 @@ select
 	Hotspots.locId,
 	--MCD12Q1Prop1.Date,
 	round((avg(MCD12Q1Prop1."(1) Barren")/avg(MCD12Q1QAQC."0")),2) as 'Barren',
+	round((avg(MCD12Q1Type3."(10) Urban and Built-up Lands")/avg(MCD12Q1QAQC."0")),2) as 'Urban',
+	round((avg(MCD12Q1Type3."(1) Grasslands")/avg(MCD12Q1QAQC."0")),2) as 'Grasslands',
+	round((avg(MCD12Q1Type3."(4) Savannas")/avg(MCD12Q1QAQC."0")),2) as 'Savannas',
 	round((avg(MCD12Q1Prop1."(3) Water Bodies")/avg(MCD12Q1QAQC."0")),2) as 'waterBodies',
 	round((avg(MCD12Q1Prop1."(11) Evergreen Needleleaf Forest")/avg(MCD12Q1QAQC."0")),2) as 'evergreenNeedleleafForest',
 	round((avg(MCD12Q1Prop1."(12) Evergreen Broadleaf Forests")/avg(MCD12Q1QAQC."0")),2) as 'evergreenBroadleafForests',
@@ -20,6 +23,7 @@ select
 from Hotspots 
 
 left outer join "MCD12Q1-006-LC-Prop1-Statistics.csv_MODIS_cooking" as MCD12Q1Prop1 on Hotspots.locId = MCD12Q1Prop1.locId
+left outer join "MCD12Q1-006-LC-Type3-Statistics.csv_MODIS_cooking" as MCD12Q1Type3 on Hotspots.locId = MCD12Q1Type3.locId
 left outer join "MCD12Q1-006-QC-Statistics-QA.csv_MODIS_cooking" as MCD12Q1QAQC on MCD12Q1Prop1.locId = MCD12Q1QAQC.locId
 
 
