a64f028ebd58d25727d1ec3be3ae9658ead08f5c
tanag3r <luke.wylie@comcast.net>
2022-03-11 14:42:45 -0800

    updated to support frequency masking

--- sqlite_scripts/createView_coefficientsBySpecies.sql
+++ sqlite_scripts/createView_coefficientsBySpecies.sql
@@ -1,14 +1,15 @@
-CREATE VIEW 'coefficients_bySpecies' AS
+--DROP VIEW "main"."coefficients_bySpecies";
+--CREATE VIEW 'coefficients_bySpecies' AS
 select
 	comName,
 	locId,
 	count(subId) as 'uniqueSightings',
 	round(avg(howMany),2) as 'averageVolume',
-	--strftime('%Y',obsDt) as 'year',
-	(SELECT COUNT(DISTINCT(strftime('%Y',obsDt))) FROM historicObservations as hz WHERE hz.comName=HsOb.comName and hz.locId=HsOb.locId) as 'YEARfrequency',
+	strftime('%Y',obsDt) as 'year',
+	--(SELECT COUNT(DISTINCT(strftime('%Y',obsDt))) FROM historicObservations as hz WHERE hz.comName=HsOb.comName and hz.locId=HsOb.locId) as 'YEARfrequency',
 	(SELECT COUNT(DISTINCT(strftime('%W',obsDt))) FROM historicObservations as hz WHERE hz.comName=HsOb.comName and hz.locId=HsOb.locId) as 'WEEKfrequency'
 
 from historicObservations as HsOb
 
-group by comName,locId--,strftime('%Y',obsDt)
+group by comName,locId,strftime('%Y',obsDt)
 ;
