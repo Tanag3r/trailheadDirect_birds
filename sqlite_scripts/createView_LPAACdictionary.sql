f950dae2788bef0c022956385c0152d1a36a6fae
tanag3r <luke.wylie@comcast.net>
2022-04-06 15:03:15 -0700

    updated to take fpar, lai

--- sqlite_scripts/createView_LPAACdictionary.sql
+++ sqlite_scripts/createView_LPAACdictionary.sql
@@ -1,3 +1,4 @@
+DROP VIEW "main"."lpdaac_dictionary";
 CREATE VIEW 'lpdaac_dictionary'
 AS
 
@@ -16,7 +17,13 @@ select
 		when File like 'LC\_Type3%' escape '\' then 'LC_Type3' 
 		end as 'LPDAACkey' 
 from "MCD12Q1-006-LC-Type3-Statistics.csv_MODIS_cooking"
-;
-
-
-
+UNION
+select
+	locId,Dataset as 'File',
+	case
+		when Dataset like 'Fpar\_500m%' escape '\' then 'Fpar_500m'
+		when Dataset like 'FparStdDev\_500m%' escape '\' then 'FparStdDev_500m'
+		when Dataset like 'Lai\_500m%' escape '\' then 'Lai_500m'
+		when Dataset like 'LaiStdDev\_500m%' escape '\' then 'LaiStdDev_500m'
+		end as 'LPDAACkey'
+from "MOD15A2H-006-Statistics.csv_MODIS_cooking";
