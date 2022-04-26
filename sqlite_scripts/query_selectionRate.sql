e05eca1df3fb915999b424b424a92eec953a7539
tanag3r <luke.wylie@comcast.net>
2022-04-26 14:30:29 -0700

    passed in selection ratio algebra

--- sqlite_scripts/query_selectionRate.sql
+++ sqlite_scripts/query_selectionRate.sql
@@ -1,13 +1,15 @@
 with habitatAbundance as (
 select
-	obsDt,
 	(strftime('%Y',obsDt)||'-'||strftime('%W',obsDt)) as "year_week",
-	round(avg(howMany),0) as "volume",
+	round(avg(howMany),0) as "habitatVolume",
+	(total(round(avg(howMany),0)) over(partition by strftime('%Y',obsDt)||'-'||strftime('%W',obsDt))) as "volume_allHabitats",
 	habitatLabel 
 from historicObservations as hsob
 left join (select locId,habitatLabel from Hotspots) as "labels" on hsob.locId=labels.locId
 where speciesCode = 'westan'
-group by year_week,habitatLabel)
-select year_week,volume from habitatAbundance
+group by year_week,habitatLabel
+)
+select year_week,round((habitatVolume/volume_allHabitats),2) as "selectionRatio_"
+from habitatAbundance
 where habitatLabel = 2
 ;
