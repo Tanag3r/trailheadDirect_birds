b332b826e6260a0e6796b884d3e6b2fd0ad55c36
tanag3r <luke.wylie@comcast.net>
2022-04-28 10:18:07 -0700

    updated to support arima

--- sqlite_scripts/query_selectionRate.sql
+++ sqlite_scripts/query_selectionRate.sql
@@ -1,5 +1,6 @@
 with habitatAbundance as (
 select
+	date(obsDt) as "obsDt",
 	(strftime('%Y',obsDt)||'-'||strftime('%W',obsDt)) as "year_week",
 	round(avg(howMany),0) as "habitatVolume",
 	(total(round(avg(howMany),0)) over(partition by strftime('%Y',obsDt)||'-'||strftime('%W',obsDt))) as "volume_allHabitats",
@@ -9,7 +10,7 @@ left join (select locId,habitatLabel from Hotspots) as "labels" on hsob.locId=la
 where speciesCode = 'westan'
 group by year_week,habitatLabel
 )
-select year_week,round((habitatVolume/volume_allHabitats),2) as "selectionRatio_"
+select obsDt,year_week,habitatVolume,round((habitatVolume/volume_allHabitats),2) as "selectionRatio_"
 from habitatAbundance
 where habitatLabel = 2
 ;
